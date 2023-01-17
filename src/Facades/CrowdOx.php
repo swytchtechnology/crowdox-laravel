ced163f299a8045158b830126bdebf262cec3ac4
Toby Twigger <tobytwigger1@gmail.com>
2023-01-17 11:12:35 +0000

    Update CrowdOx.php

--- src/Facades/CrowdOx.php
+++ src/Facades/CrowdOx.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static void freshClient() Create a fresh client instance
- * @method static \edh649\CrowdOx\Resources\States states()
- * @method static \edh649\CrowdOx\Resources\Projects projects()
- * @method static \edh649\CrowdOx\Resources\ProductVariations product_variations()
- * @method static \edh649\CrowdOx\Resources\OrderTransactions order_transactions()
- * @method static \edh649\CrowdOx\Resources\OrderTags order_tags()
- * @method static \edh649\CrowdOx\Resources\OrderSelections order_selections()
- * @method static \edh649\CrowdOx\Resources\Orders orders()
- * @method static \edh649\CrowdOx\Resources\OrderLines order_lines()
- * @method static \edh649\CrowdOx\Resources\OrderAddresses order_addresses()
- * @method static \edh649\CrowdOx\Resources\Customers customers()
- * @method static \edh649\CrowdOx\Resources\Countries countries()
- * @method static \edh649\CrowdOx\Resources\Products products()
+ * @method static \edh649\CrowdOx\Resources\States states(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\Projects projects(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\ProductVariations product_variations(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\OrderTransactions order_transactions(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\OrderTags order_tags(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\OrderSelections order_selections(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\Orders orders(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\OrderLines order_lines(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\OrderAddresses order_addresses(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\Customers customers(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\Countries countries(string|int $resource_id = null)
+ * @method static \edh649\CrowdOx\Resources\Products products(string|int $resource_id = null)
  */
 class CrowdOx extends Facade {
 
