675249b2a1469c53f4ab82d075df71b41aed38f0
Toby Twigger <tobytwigger1@gmail.com>
2023-01-17 10:16:19 +0000

    Add docblocks to the facade

--- src/Facades/CrowdOx.php
+++ src/Facades/CrowdOx.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void freshClient() Create a fresh client instance
+ * @method static \edh649\CrowdOx\Resources\States states()
+ * @method static \edh649\CrowdOx\Resources\Projects projects()
+ * @method static \edh649\CrowdOx\Resources\ProductVariations product_variations()
+ * @method static \edh649\CrowdOx\Resources\OrderTransactions order_trgetValidResourcesansactions()
+ * @method static \edh649\CrowdOx\Resources\OrderTags order_tags()
+ * @method static \edh649\CrowdOx\Resources\OrderSelections order_selections()
+ * @method static \edh649\CrowdOx\Resources\Orders orders()
+ * @method static \edh649\CrowdOx\Resources\OrderLines order_lines()
+ * @method static \edh649\CrowdOx\Resources\OrderAddresses order_addresses()
+ * @method static \edh649\CrowdOx\Resources\Customers customers()
+ * @method static \edh649\CrowdOx\Resources\Countries countries()
+ * @method static \edh649\CrowdOx\Resources\Products products()
+ */
 class CrowdOx extends Facade {
 
     /**
